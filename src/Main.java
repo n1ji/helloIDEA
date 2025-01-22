d93ad0adc67bf87f94dcbbcb9910cffed89c73d4
Myra Rudy <0.maysin.0@gmail.com>
2025-01-22 13:38:45 -0800

    Scanner input from user

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+
+        int num;
+
+        System.out.print("What is your name?\n");
+        String name = scanner.nextLine();
+        System.out.print("What is your favorite number?\n");
+
+        try {
+            num = scanner.nextInt();
+            System.out.println("Hi " + name + "! " + num + " is a great number!");
+        }
+        catch(Exception e) {
+            System.out.print("Please enter a number.");
+        } // End try-catch block
+    } // End constructor
+} //Myra Rudy
